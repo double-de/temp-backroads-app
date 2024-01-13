35b51fe87ae97f7ca2c3a7670f8d57e8ceffd931
double-de <dominik.dzurek1@gmail.com>
2024-01-13 23:56:16 +0100

    first commit

---
+++ src/components/PageLink.js
@@ -0,0 +1,13 @@
+const PageLink = ({ link, itemClass }) => {
+  return (
+    <>
+      <li>
+        <a href={link.href} className={itemClass}>
+          {link.text}
+        </a>
+      </li>
+    </>
+  )
+}
+
+export default PageLink
